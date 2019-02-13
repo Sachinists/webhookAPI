f06a8fb9134001653d2763e93a7c2dd957baff64
Debjyoti Pandit <debjyoti.pandit@ad.infosys.com>
2019-02-13 17:32:28 +0530

    adding simple http get post request

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {  
+  let body = req.body;
+   console.log(JSON.stringify(body));
+    res.status(200).send('Event received');
+});
+
+app.get('/webhook', (req, res) => {
+    res.status(200).send("Get request received.");
+  });
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
